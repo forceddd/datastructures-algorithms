59b501bcdfc5c5a7d0cc51c95bc963e76aed97a2
jxdong <jxdong.amarsoft.com>
2020-12-24 16:53:49 +0800

    修改了knapsack的部分代码，返回所有物品

--- algorithms/greedy-method/knapsack.js
+++ algorithms/greedy-method/knapsack.js
@@ -1,21 +1,23 @@
 //背包问题 分数版本
-//从每一分重量最大的开始逐次遍历 value/weight
+//从goods中每一份重量最大的开始逐次遍历 value/weight 
 export const knapsack=(capacity,goods)=>{
     const {length}=goods;
+    const res={};
     let currentWeight=0,currentValue=0;
     // 根据价值/重量比 对goods进行排序 降序
     goods.sort((a, b) => b.value / b.weight - a.value / a.weight);
     for (let i = 0; i < length && currentWeight < capacity;i++){
-        //该物品还能完整的放入背包中
         const {weight,value}=goods[i];
-        if (weight <= capacity - currentWeight){
+        //r>=1 该物品还能完整的放入背包中 r<1 要使用分数
+        const r = (capacity - currentWeight) / weight;
+        if (r>=1){
             currentValue+=value;
-            currentWeight += weight
+            currentWeight += weight;
         }else{
-            const r=(capacity-currentWeight)/weight;
             currentValue+=r*value;
             currentWeight+=r*weight
         }
+        res[i] = { stuff: goods[i], r: r>=1?1:r }
     }
-    return currentValue
+    return {goods:res,maxValue:currentValue}
 }
